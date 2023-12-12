424368989b653d1a3771862dc3e67fdca519e3ad
psDell <pawel.stolka@gmail.com>
2023-12-12 19:54:59 +0100

    home, @if etc

--- apps/crown/src/app/home/home.component.ts
+++ apps/crown/src/app/home/home.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'home',
   template: `
-    <div class="container">
-      <div id="welcome">
-        <h3>
-          <span> Welcome in Crown (reactive-monorepo) 👋 </span>
-        </h3>
-      </div>
+    <div class="title">Witaj w Crownie 👋</div>
+
+    <div class="info">
+      To appka stworzona przeze mnie na prywatne potrzeby analizy wydatków
+      <div class="ps">(na podstawie kursu Crown)</div>
+    </div>
+
+    <div class="intro">
+      Na początek wypróbuj ją na koncie testowym
+      <span class="highlight"> zaloguj się ☝️</span>
     </div>
   `,
   styleUrls: ['./home.component.scss'],
--- libs/money/src/lib/containers/tabs-container/tabs-container.component.ts
+++ libs/money/src/lib/containers/tabs-container/tabs-container.component.ts
@@ -1,12 +1,4 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  Inject,
-  LOCALE_ID,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component, Inject, LOCALE_ID, ViewChild } from '@angular/core';
 import { CommonModule, formatNumber } from '@angular/common';
 import { MaterialModule } from '@crown/material';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
@@ -26,8 +18,15 @@ import { MatSort } from '@angular/material/sort';
 import { AddDialogComponent } from '../../components/dialogs/add-money-dialog/add-money-dialog.component';
 import { DeleteDialogComponent } from '../../components/dialogs/delete-money-dialog/delete-money-dialog.component';
 import { EditMoneyDialog } from '../../components/dialogs/edit-money-dialog/edit-money-dialog.component';
-import { ToastService } from '@crown/ui';
 
+const COLUMNS_RENDERED = [
+  'createdAt',
+  'type',
+  'price',
+  'fromWho',
+  // 'updatedAt'
+  'action',
+];
 @Component({
   selector: 'crown-tabs-container',
   standalone: true,
@@ -35,23 +34,28 @@ import { ToastService } from '@crown/ui';
   templateUrl: './tabs-container.component.html',
   styleUrl: './tabs-container.component.scss',
 })
-export class TabsContainerComponent implements OnInit, AfterViewInit {
+export class TabsContainerComponent {
   dataSource!: MatTableDataSource<Money>;
+  columns = COLUMNS_RENDERED;
+  GROUPS_LABEL = 'W MIESIĄCACH';
+  DETAILS_LABEL = 'DETALE';
+
+  pageSizeOptions = [5, 10, 25];
+  pageSize = this.pageSizeOptions[1];
+
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort = new MatSort();
 
   money$ = this.moneyService.money$.pipe(
     tap((data) => {
       this.dataSource = new MatTableDataSource(data);
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
-      console.log('[data!]', data);
     })
   );
 
-  moneyGroups$ = this.moneyService.moneyGroups$.pipe(
-    map((groups) => groups.sort(compareBy('period', true)))
-  );
-
-  monthsData$ = this.moneyGroups$.pipe(
+  monthsData$ = this.moneyService.moneyGroups$.pipe(
+    map((groups) => groups.sort(compareBy('period', true))),
     map((moneyGroups) => {
       const typePrices = groupTypePrices(moneyGroups);
 
@@ -71,82 +75,18 @@ export class TabsContainerComponent implements OnInit, AfterViewInit {
     })
   );
 
-  pageSizeOptions = [5, 10, 25];
-  pageSize = this.pageSizeOptions[1];
-
-  columns = [
-    'createdAt',
-    'type',
-    'price',
-    'fromWho',
-    // 'updatedAt'
-    'action',
-  ];
-
-  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
-  @ViewChild(MatSort, { static: false }) sort: MatSort = new MatSort();
-  @ViewChild('table') table: any;
-
   constructor(
     @Inject(LOCALE_ID) private locale: string,
     private dialog: MatDialog,
-    private moneyService: MoneyService, // TODO: private toastService: ToastService
-    private cdr: ChangeDetectorRef
-  ) {
-    this.dataSource = new MatTableDataSource([] as Money[]);
-
-    // this.dataSource.sort = this.sort;
-    // this.dataSource.paginator = this.paginator;
-    // console.log('this.dataSource | CTOR', this.dataSource);
-  }
+    private moneyService: MoneyService // TODO: private toastService: ToastService
+  ) {}
 
   onTabChanged(index: number) {
-    // Check if the tab with your table is the one being activated
-    console.log('[this.onTabChanged]', index);
     if (index === 1) {
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
-
-      // setTimeout(() => {
-      //   this.dataSource.paginator = this.paginator;
-      //   // this.initializePaginator();
-      // });
     }
   }
-
-  initializePaginator() {
-    if (this.table && this.table._dataSource) {
-      this.table._dataSource.paginator = this.paginator;
-      console.log('[initializePaginator] PAGINATOR', this.paginator);
-    } else {
-      console.log('[initializePaginator] ELSE PAGINATOR', this.paginator);
-      this.dataSource.paginator = this.paginator;
-    }
-  }
-
-  ngAfterViewInit(): void {
-    // The async pipe updates the table's dataSource, but you need to manually
-    // update the paginator for this dataSource.
-    /*
-    setTimeout(() => {
-      this.initializePaginator();
-      // if (this.table && this.table._dataSource) {
-      //   console.log('[ngAfterViewInit] PAGINATOR', this.paginator);
-      //   this.table._dataSource.paginator = this.paginator;
-      //   this.cdr.detectChanges();
-      // } else {
-      //   console.log('[ngAfterViewInit] NO-PAGINATOR');
-      // }
-    });*/
-    // if (this.paginator) {
-    //   this.dataSource.paginator = this.paginator;
-    //   console.log('[ngAfterViewInit] PAGINATOR', this.paginator);
-    // } else {
-    //   console.log('[ngAfterViewInit] NO-PAGINATOR');
-    // }
-  }
-
-  ngOnInit(): void {}
 
   getPriceByType(typePrices: any[], type: string): number | string {
     const found = typePrices.find((tp) => tp.type === type);
@@ -156,61 +96,39 @@ export class TabsContainerComponent implements OnInit, AfterViewInit {
 
   add() {
     const dialogRef = this.dialog.open(AddDialogComponent, dialogConfig);
-
     this.handleDialog(dialogRef);
   }
 
   edit(money: Money) {
-    //TODO: this.toast()
     dialogConfig.data = money;
     const dialogRef = this.dialog.open(EditMoneyDialog, dialogConfig);
-
-    // this.handleDialog(dialogRef);
-    dialogRef
-      .afterClosed()
-      .pipe(
-        tap((x) => console.log('[this.edit]', x)),
-        // tap((_) => this.showInfo()),
-        filter((val) => !!val)
-      )
-      .subscribe((_) => {
-        console.log('[this.edit sub]', _);
-      });
+    this.handleDialog(dialogRef);
   }
 
   remove(id: number) {
     dialogConfig.data = id;
     const dialogRef = this.dialog.open(DeleteDialogComponent, dialogConfig);
-
     this.handleDialog(dialogRef);
   }
 
   private handleDialog(dialogRef: MatDialogRef<any>) {
     dialogRef
       .afterClosed()
-      .pipe(
-        filter((val) => !!val),
-        tap((x) => console.log('[this.handleDialog]', x))
-      )
+      .pipe(filter((val) => !!val))
       .subscribe((_) => {
-        console.log('[this.handleDialog]', _);
         // this.toast();
       });
   }
 
   formatValue(value: number | string): string {
-    if (typeof value === 'number') {
-      return formatNumber(value, this.locale, '1.0-0');
-    } else {
-      return value;
-    }
+    return typeof value === 'number'
+      ? formatNumber(value, this.locale, '1.0-0')
+      : value;
   }
 }
 
 function groupTypePrices(moneyGroups: MoneyGroup[]) {
-  // Group by 'type'
   const flatTypePrices = moneyGroups.map((x) => x.typePrices).flat();
-
   const groupedByType = flatTypePrices.reduce((acc, item) => {
     // Initialize the price with 0
     if (!acc[item.type]) {
@@ -231,7 +149,6 @@ function groupTypePrices(moneyGroups: MoneyGroup[]) {
 
 function uniqueCategories(moneyGroups: MoneyGroup[]) {
   const flatTypePrices = moneyGroups.flatMap((a) => a.typePrices);
-  // console.log('[MONEY_GROUPS]', moneyGroups, flatTypePrices);
 
   const priceSumByType = flatTypePrices.reduce((sum: any, cur) => {
     sum[cur.type] = (sum[cur.type] || 0) + cur.price;
