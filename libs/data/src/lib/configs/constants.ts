985b0fe1065f9ea7eb3e35a833ec464ffbf6ef38
psDell <pawel.stolka@gmail.com>
2023-12-16 15:43:53 +0100

    SWITCH to aws url

--- libs/data/src/lib/configs/constants.ts
+++ libs/data/src/lib/configs/constants.ts
@@ -2,10 +2,10 @@
 // export const API_URL = 'http://localhost:3000';
 
 // local dev
-export const API_URL = 'http://localhost:3001';
+// export const API_URL = 'http://localhost:3001';
 
 // PROD
-// export const API_URL = 'http://nest-demo-beanstalk-env.eba-apndeyg2.eu-west-1.elasticbeanstalk.com';
+export const API_URL = 'http://nest-demo-beanstalk-env.eba-apndeyg2.eu-west-1.elasticbeanstalk.com';
 
 export const AUTH_DATA = 'auth_data';
 export const AUTH_TOKEN = 'auth_token';
