43a4f41467aeaafcd4c719780f7692325a6a518a
psDell <pawel.stolka@gmail.com>
2023-12-21 11:04:50 +0100

    [draft] providers in dialog works...

--- apps/crown/src/app/app.component.ts
+++ apps/crown/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
 import { MatButtonModule } from '@angular/material/button';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDrawerMode, MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@crown/material';
 import { MainToolbarComponent, NavigationComponent, ToastService } from '@crown/ui';
 import { TodoContainerComponent } from 'libs/todo/src/lib/container/todo-container.component';
 import { TodoService } from 'libs/todo/src/lib/services/todo.service';
+
+import 'moment/locale/pl';
 
 @Component({
   standalone: true,
@@ -19,6 +23,15 @@ import { TodoService } from 'libs/todo/src/lib/services/todo.service';
     MatSidenavModule,
     MatButtonModule,
     TodoContainerComponent
+  ],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'pl'},
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
   ],
   selector: 'crown-root',
   templateUrl: './app.component.html',
