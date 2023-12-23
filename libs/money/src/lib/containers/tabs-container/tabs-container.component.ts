69841a3452cc1387ac5a2f8611691d7a07b77b8a
psDell <pawel.stolka@gmail.com>
2023-12-23 07:43:39 +0100

    form decalarations

--- libs/money/src/lib/containers/tabs-container/tabs-container.component.ts
+++ libs/money/src/lib/containers/tabs-container/tabs-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, LOCALE_ID, ViewChild } from '@angular/core';
+import { Component, Inject, LOCALE_ID, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@crown/material';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
@@ -33,7 +33,6 @@ import { DetailsTabComponent } from '../../components/tabs/details-tab/details-t
 import { YearFilterComponent } from '../../components/year-filter/year-filter.component';
 import { DataFilterComponent } from '../../components/data-filter/data-filter.component';
 
-
 const GROUPS_LABEL = 'GRUPY - MIESIĄCAMI';
 const DETAILS_LABEL = 'WSZYSTKO';
 @Component({
@@ -58,7 +57,6 @@ export class TabsContainerComponent {
   pageSizeOptions = [5, 10, 25];
   pageSize = this.pageSizeOptions[0];
 
-  availableYears: number[] = [];
   availableYears$ = this.moneyService.availableYears$;
 
   @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
@@ -69,93 +67,104 @@ export class TabsContainerComponent {
   private _filterPhraseSubj = new BehaviorSubject<string>(EMPTY_STRING);
   filterPhrase$ = this._filterPhraseSubj.asObservable();
 
-  yearMoney$ = this.moneyService.yearMoney$.pipe(
-    tap((data) => {
-      this.dataSource = new MatTableDataSource(data);
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    })
-  );
-
-  monthsData$ = this.moneyService.moneyGroups$.pipe(
-    map((groups) => groups.sort(compareBy('period', true))),
-    map((moneyGroups) => {
-      const typePrices = groupTypePrices(moneyGroups);
-
-      const summary: MoneyGroup = {
-        period: EMPTY_STRING,
-        userId: EMPTY_STRING,
-        typePrices,
-      };
-
-      const months = [...moneyGroups, summary];
-      const categories = uniqueCategories(moneyGroups);
-
-      return {
-        months,
-        categories,
-      };
-    })
-  );
-
-  yearMonthsData$ = combineLatest([
-    this.monthsData$.pipe(map(({ months }) => months)),
-    this.selectedYear$,
-    this.filterPhrase$,
-  ]).pipe(
-    map(([months, year, filterPhrase]) => {
-      const monthsByYear = months.filter(
-        ({ period }) => getYear(period) === year
-      );
-
-      const monthsByFilter: (MoneyGroup | null)[] = monthsByYear
-        .map((month) => {
-          // 1.Filter typePrices to include only those that match the filterPhrase
-          const typePricesByPhrase = month.typePrices.filter(({ type }) =>
-            type.includes(filterPhrase)
-          );
-          // 2.If there are any filtered typePrices, return a new object with updated sum
-          if (typePricesByPhrase.length > 0) {
-            return {
-              ...month,
-              typePrices: typePricesByPhrase,
-              sum: typePricesByPhrase.reduce((acc, cur) => acc + cur.price, 0),
-            };
-          }
-          // 3.If no typePrices match, return null (to be filtered out)
-          return null;
-        })
-        .filter((month) => month !== null); // Filter out the nulls
-
-      const typePrices = groupTypePrices(monthsByFilter);
-      const summary: Partial<MoneyGroup> = {
-        typePrices,
-      };
-      const _months = [...monthsByFilter, summary];
-
-      const _types = months
-        .map(({ typePrices }) => typePrices)
-        .flat()
-        .sort((a, b) => b.price - a.price)
-        .map(({ type }) => type);
-
-      const types = [...new Set(_types)];
-      const categories = types.filter((c: string | any[]) =>
-        c.includes(filterPhrase)
-      );
-      return {
-        months: _months,
-        categories,
-        total: categories?.length,
-      };
-    })
-  );
+  yearMoney$: Observable<Money[]>;
+  monthsData$: Observable<{ months: MoneyGroup[]; categories: string[] }>;
+  yearMonthsData$: Observable<{
+    months: (Partial<MoneyGroup> | null)[];
+    categories: string[];
+    total: number;
+  }>;
 
   constructor(
     @Inject(LOCALE_ID) public locale: string,
     private dialog: MatDialog,
     private moneyService: MoneyService // TODO: private toastService: ToastService
-  ) {}
+  ) {
+    this.yearMoney$ = this.moneyService.yearMoney$.pipe(
+      tap((data) => {
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      })
+    );
+
+    this.monthsData$ = this.moneyService.moneyGroups$.pipe(
+      map((groups) => groups.sort(compareBy('period', true))),
+      map((moneyGroups) => {
+        const typePrices = groupTypePrices(moneyGroups);
+
+        const summary: MoneyGroup = {
+          period: EMPTY_STRING,
+          userId: EMPTY_STRING,
+          typePrices,
+        };
+
+        const months = [...moneyGroups, summary];
+        const categories = uniqueCategories(moneyGroups);
+
+        return {
+          months,
+          categories,
+        };
+      })
+    );
+
+    this.yearMonthsData$ = combineLatest([
+      this.monthsData$.pipe(map(({ months }) => months)),
+      this.selectedYear$,
+      this.filterPhrase$,
+    ]).pipe(
+      map(([months, year, filterPhrase]) => {
+        const monthsByYear = months.filter(
+          ({ period }) => getYear(period) === year
+        );
+
+        const monthsByFilter: (MoneyGroup | null)[] = monthsByYear
+          .map((month) => {
+            // 1.Filter typePrices to include only those that match the filterPhrase
+            const typePricesByPhrase = month.typePrices.filter(({ type }) =>
+              type.includes(filterPhrase)
+            );
+            // 2.If there are any filtered typePrices, return a new object with updated sum
+            if (typePricesByPhrase.length > 0) {
+              return {
+                ...month,
+                typePrices: typePricesByPhrase,
+                sum: typePricesByPhrase.reduce(
+                  (acc, cur) => acc + cur.price,
+                  0
+                ),
+              };
+            }
+            // 3.If no typePrices match, return null (to be filtered out)
+            return null;
+          })
+          .filter((month) => month !== null); // Filter out the nulls
+
+        const typePrices = groupTypePrices(monthsByFilter);
+        const summary: Partial<MoneyGroup> = {
+          typePrices,
+        };
+        const _months = [...monthsByFilter, summary];
+
+        const _types = months
+          .map(({ typePrices }) => typePrices)
+          .flat()
+          .sort((a, b) => b.price - a.price)
+          .map(({ type }) => type);
+
+        const types = [...new Set(_types)];
+        const categories = types.filter((c: string | any[]) =>
+          c.includes(filterPhrase)
+        );
+        return {
+          months: _months,
+          categories,
+          total: categories?.length,
+        };
+      })
+    );
+  }
 
   tabChange(index: number) {
     if (index === 1) {
@@ -171,10 +180,6 @@ export class TabsContainerComponent {
   applyFilter(filter: string) {
     this.dataSource.filter = filter;
     this._filterPhraseSubj.next(filter);
-
-    // if (this.dataSource.paginator) {
-    //   this.dataSource.paginator.firstPage();
-    // }
   }
 
   add() {
