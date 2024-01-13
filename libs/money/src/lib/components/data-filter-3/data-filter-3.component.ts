63eba79ef094d22a308a61ecc875b2053ab11bf2
psDell <pawel.stolka@gmail.com>
2024-01-13 09:32:39 +0100

    filtr jest ok

--- libs/money/src/lib/components/data-filter-3/data-filter-3.component.ts
+++ libs/money/src/lib/components/data-filter-3/data-filter-3.component.ts
@@ -7,6 +7,7 @@ import {
 import { CommonModule } from '@angular/common';
 import { BehaviorSubject } from 'rxjs';
 import { MaterialModule } from '@crown/material';
+import { EMPTY_STRING } from '@crown/data';
 
 @Component({
   selector: 'crown-data-filter-3',
@@ -20,12 +21,10 @@ export class DataFilter3Component {
   expanded = false;
   clearClicked = false;
 
-  toggleExpanded() {
-    this.expanded = !this.expanded;
-  }
-
-  private _filterSubj = new BehaviorSubject<string>('');
+  private _filterSubj = new BehaviorSubject<string>(EMPTY_STRING);
   filterValue$ = this._filterSubj.asObservable();
+
+  hasContent: boolean = false;
 
   @Output() filter = new EventEmitter();
   filterValue: any;
@@ -34,60 +33,21 @@ export class DataFilter3Component {
     const inputElement = event.target as HTMLInputElement;
     const filterValue = inputElement
       ? inputElement.value.trim().toLowerCase()
-      : '';
+      : EMPTY_STRING;
     this.filterValue = filterValue.trim().toLowerCase();
     this._filterSubj.next(filterValue);
     this.filter.emit(filterValue);
 
-    // Check if the input is empty and collapse if necessary
-    if (this.filterValue === '') {
-      this.expanded = false;
-    }
-  }
-
-  preventBlur(event: MouseEvent) {
-    event.preventDefault();
-    event.stopPropagation();
+    this.hasContent = filterValue !== EMPTY_STRING;
   }
 
   clearSearch(event: MouseEvent) {
     event.stopPropagation();
     this.clearClicked = true;
-    console.log('[clearFilter 2] clicked');
 
-    this.filterValue = '';
+    this.filterValue = EMPTY_STRING;
     this._filterSubj.next(this.filterValue);
     this.filter.emit(this.filterValue);
-
-    this.expanded = false;
-    // Delay collapsing to ensure it occurs after blur event
-    // setTimeout(() => {
-    //   if (this.expanded) {
-    //     this.toggleExpanded();
-    //   }
-    // }, 0);
+    this.hasContent = this.filterValue !== EMPTY_STRING;
   }
-
-  handleBlur() {
-    // Delay collapsing to allow for checking if clear was clicked
-    setTimeout(() => {
-      if (!this.clearClicked && this.expanded) {
-        this.toggleExpanded();
-      }
-      this.clearClicked = false;
-    }, 0);
-  }
-
-  // clearSearch(event: MouseEvent) {
-  //   event.stopPropagation();
-  //   console.log('[clearFilter] clicked');
-
-  //   this.filterValue = '';
-  //   this._filterSubj.next(this.filterValue);
-  //   this.filter.emit(this.filterValue);
-
-  //   if (this.expanded) {
-  //     this.toggleExpanded();
-  //   }
-  // }
 }
