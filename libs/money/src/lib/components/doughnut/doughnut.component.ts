00593274fce5f1de0badb4d53c88f2aeab5bec5c
psDell <pawel.stolka@gmail.com>
2024-01-19 19:22:02 +0100

    quite ok - chart size is changing

--- libs/money/src/lib/components/doughnut/doughnut.component.ts
+++ libs/money/src/lib/components/doughnut/doughnut.component.ts
@@ -1,13 +1,13 @@
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
-  ElementRef,
   Input,
-  ViewChild,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartModule } from 'primeng/chart';
+import { TypePrice } from '@crown/data';
 
 @Component({
   selector: 'crown-doughnut',
@@ -17,61 +17,84 @@ import { ChartModule } from 'primeng/chart';
   styleUrl: './doughnut.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DoughnutComponent {
-  @Input() data: any[] = [];
-  _data: any;
+export class DoughnutComponent implements OnChanges {
+  @Input() dataset: any;
+  @Input() aspectRatio: number = 500 / 800;
 
-  options: any;
+  data: any;
+  documentStyle = getComputedStyle(document.documentElement);
+  color = this.documentStyle.getPropertyValue('--text-color');
 
-  ngOnInit() {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
+  options = {
+    cutout: '50%',
+    // responsive: true,
+    maintainAspectRatio: false,
+    aspectRatio: this.aspectRatio,
+    plugins: {
+      legend: {
+        labels: {
+          color: this.color,
+        },
+      },
+    },
+  };
 
-    this._data = {
-      labels: ['A', 'B', 'C', 'D', 'E', 'F', 'g', 'h', 'i', 'j'],
+  ngOnChanges(changes: SimpleChanges): void {
+    const { categories, months } = this.dataset;
+    let { typePrices } = months[months.length - 1];
+
+    this.data = {
+      labels: categories,
       datasets: [
         {
-          data: [300, 50, 10, 100, 50, 80, 150, 300, 10, 15],
-          backgroundColor: [
-            documentStyle.getPropertyValue('--red'),
-            documentStyle.getPropertyValue('--orange'),
-            documentStyle.getPropertyValue('--yellow'),
-            documentStyle.getPropertyValue('--lime'),
-            documentStyle.getPropertyValue('--green'),
-            documentStyle.getPropertyValue('--mint'),
-            documentStyle.getPropertyValue('--teal'),
-            documentStyle.getPropertyValue('--cyan'),
-            documentStyle.getPropertyValue('--lightBlue'),
-            documentStyle.getPropertyValue('--blue'),
-          ],
-          hoverBackgroundColor: [
-            documentStyle.getPropertyValue('--red'),
-            documentStyle.getPropertyValue('--orange'),
-            documentStyle.getPropertyValue('--yellow'),
-            documentStyle.getPropertyValue('--lime'),
-            documentStyle.getPropertyValue('--green'),
-            documentStyle.getPropertyValue('--mint'),
-            documentStyle.getPropertyValue('--teal'),
-            documentStyle.getPropertyValue('--cyan'),
-            documentStyle.getPropertyValue('--lightBlue'),
-            documentStyle.getPropertyValue('--blue'),
-          ],
+          data: getValues(categories, typePrices),
+          backgroundColor: generateColors(this.documentStyle),
+          hoverBackgroundColor: generateColors(this.documentStyle),
         },
       ],
     };
-
-    this.options = {
-      cutout: '50%',
-      responsive: true,
-      maintainAspectRatio: false,
-      aspectRatio: 500 / 800,
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor,
-          },
-        },
-      },
-    };
   }
+}
+
+function getValues(labels: string[], typePrices: TypePrice[]) {
+  return typePrices
+    ?.sort((a, b) => {
+      return labels.indexOf(a.type) - labels.indexOf(b.type);
+    })
+    .map((tp) => tp.price);
+}
+
+function generateColors(documentStyle: CSSStyleDeclaration) {
+  return [
+    documentStyle.getPropertyValue('--red'),
+    documentStyle.getPropertyValue('--orange'),
+    documentStyle.getPropertyValue('--yellow'),
+    documentStyle.getPropertyValue('--lime'),
+    documentStyle.getPropertyValue('--green'),
+    documentStyle.getPropertyValue('--mint'),
+    documentStyle.getPropertyValue('--teal'),
+    documentStyle.getPropertyValue('--cyan'),
+    documentStyle.getPropertyValue('--lightBlue'),
+    documentStyle.getPropertyValue('--blue'),
+    documentStyle.getPropertyValue('--indigo'),
+    documentStyle.getPropertyValue('--deepPurple'),
+    documentStyle.getPropertyValue('--purple'),
+    documentStyle.getPropertyValue('--pink'),
+    documentStyle.getPropertyValue('--hotPink'),
+    documentStyle.getPropertyValue('--warmGray'),
+    documentStyle.getPropertyValue('--gray'),
+    documentStyle.getPropertyValue('--coolGray'),
+    documentStyle.getPropertyValue('--brown'),
+    documentStyle.getPropertyValue('--burgundy'),
+    documentStyle.getPropertyValue('--olive'),
+    documentStyle.getPropertyValue('--beige'),
+    documentStyle.getPropertyValue('--coral'),
+    documentStyle.getPropertyValue('--navy'),
+    documentStyle.getPropertyValue('--maroon'),
+    documentStyle.getPropertyValue('--lightGreen'),
+    documentStyle.getPropertyValue('--turquoise'),
+    documentStyle.getPropertyValue('--skyBlue'),
+    documentStyle.getPropertyValue('--gold'),
+    documentStyle.getPropertyValue('--plum'),
+  ];
 }
