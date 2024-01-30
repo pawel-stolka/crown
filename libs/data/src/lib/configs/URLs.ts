59ae2fa22290e6c14cbaf8dd9d7296ac79e46c7d
psDell <pawel.stolka@gmail.com>
2024-01-30 13:11:08 +0100

    URL + app.version

--- libs/data/src/lib/configs/URLs.ts
+++ libs/data/src/lib/configs/URLs.ts
@@ -5,7 +5,7 @@
 // export const API_URL = 'http://localhost:3001';
 
 // NX api-server
-export const API_URL = 'http://localhost:3333';
+// export const API_URL = 'http://localhost:3333';
 
 // PROD
-// export const API_URL = 'http://nest-demo-beanstalk-env.eba-apndeyg2.eu-west-1.elasticbeanstalk.com';
+export const API_URL = 'http://nest-demo-beanstalk-env.eba-apndeyg2.eu-west-1.elasticbeanstalk.com';
--- libs/shared/src/lib/app-info/app-info.component.ts
+++ libs/shared/src/lib/app-info/app-info.component.ts
@@ -9,6 +9,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app-info.component.scss',
 })
 export class AppInfoComponent {
-  version = '1.1'
-  build = '23.01.2024'
+  version = '1.2'
+  build = '30.01.2024'
 }
